2c4b6e118d0dc9909e4feb3281d713439542f67e
Yousoffff <yousoffff.k@gmail.com>
2022-02-28 19:06:25 +0330

    set timer

---
+++ script.js
@@ -0,0 +1,20 @@
+const second = document.querySelector("#second");
+const minute = document.querySelector("#minute");
+const hour = document.querySelector("#hour");
+const day = document.querySelector("#day");
+
+const date =  new Date("1 Jan2023");
+
+function calculateTime(){
+    const currentDate = new Date();
+    const sec = (date - currentDate) /1000;
+    day.innerHTML = Math.floor(sec/86400);
+    hour.innerHTML = Math.floor(sec/3600)%24;
+    minute.innerHTML = Math.floor(sec/60)%60
+    second.innerHTML = Math.floor(sec) % 60;
+
+}
+calculateTime();
+setInterval(calculateTime,1000);
+
+
